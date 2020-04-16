cefac74d124bac92bc25cc30638936a51414f9ba
Naz-pikachu <rishabh.jain.met19@itbhu.ac.in>
2020-04-16 21:44:45 +0530

    commit

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 Drizzy Balls Animation
 This is the basic animation for understanding the concept of HTML Canvas.
 The project focus on mainly practicing Javascript,DOM & HTML canvas
-
+Added interactivity and visually appealing colors.
+Added Navbar for basic information
--- script.js
+++ script.js
@@ -36,13 +36,41 @@ animate = () => {
     y += dy;
 }
 animate();
+
 **********************************************************/
 //let's practice some basic stuff first
 var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
+var mouse = {
+    x: undefined,
+    y: undefined,
+}
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-//object blue print having draw and update methods
+var color = [
+    'rgba(217, 201, 67,0.6)',
+    'rgba(227, 113, 70,0.6)',
+    'rgba(154, 73, 204,0.6)',
+    'rgba(70, 194, 227,0.6)',
+    'rgba(115, 219, 68,0.6)'];
+
+
+//lets add some interactivity to our animation
+window.addEventListener('mousemove',
+    function (event) {
+        mouse.x = event.x;
+        mouse.y = event.y;
+
+    }
+)
+window.addEventListener('resize', function (event) {
+    canvas.width = this.innerWidth;
+    canvas.height = this.innerHeight;
+    fillExtra();
+
+}
+)
+
 class circle {
     constructor(x, y, dx, dy, r) {
         this.x = x;
@@ -50,57 +78,82 @@ class circle {
         this.r = r;
         this.dx = dx;
         this.dy = dy;
+        this.MinRadius = this.r;
+        this.MaxRadius = 300;
+        this.color = color[Math.floor(Math.random() * 5)];
     }
+
     //this will draw a circle on canvas
     draw() {
         c.beginPath();
-        c.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
-        c.fillStyle = "orange";
+        c.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);         // c.bezierCurveTo(innerWidth / 2, innerHeight / 2, this.x, this.y, 10, 10);
+        c.fillStyle = this.color;
         c.fill();
         c.strokeStyle = "red";
         c.stroke();
     }
-    //this will refresh and update the canvas
+    //this will refresh and update the canvas 
     update() {
 
-        // c.clearRect(0, 0, canvas.width, canvas.height);
+        //c.clearRect(0, 0, canvas.width, canvas.height);   
+        //console.log(Math.sqrt(Math.pow((mouse.x - this.x), 2) + Math.pow((mouse.y - this.y), 2)));
+        // Math.abs(mouse.x - this.x);
+
         if (this.x > canvas.width - this.r || this.x < this.r)
             this.dx = -this.dx;
         if (this.y > canvas.height - this.r || this.y < this.r)
             this.dy = -this.dy;
 
-        this.x += this.dx;
         this.y += this.dy;
+        this.x += this.dx;
+
+        if ((mouse.x - this.x) < 150 && (mouse.x - this.x) > -150 &&
+            (mouse.y - this.y) < 150 && (mouse.y - this.y) > -150) {
+            if (this.r < this.MaxRadius)
+                this.r += 20;
+
+        }
+        else if (this.r > this.MinRadius)
+            this.r--;
+
         this.draw();
     }
 }
-var cir = new circle(100, 100, 4, 4, 30);
+
+//var cir = new circle(100, 100, 4, 4, 30); //just for debugging purpose
 //Creates an array of objects
+
 var objectArray = [];
-for (var i = 0; i < 100; i++) {
-    var r = Math.random() * 30;
+for (var i = 0; i < 200; i++) {
+    var r = 1 + Math.random() * 20;
     var x = Math.random() * canvas.width;
     var y = Math.random() * canvas.height;
-    var dx = (Math.random() - 0.5) * 20;
-    var dy = (Math.random() - 0.5) * 20;
+    var dx = (Math.random() - 0.5);
+    var dy = (Math.random() - 0.5);
     objectArray.push(new circle(x, y, dx, dy, r));
 }
-//console.log(objectArray[0]);
-
+//to fill extra space on resizing of the browser
+fillExtra = () => {
+    objectArray = [];
+    for (var i = 0; i < 200; i++) {
+        var r = 1 + Math.random() * 20;
+        var x = Math.random() * canvas.width;
+        var y = Math.random() * canvas.height;
+        var dx = (Math.random() - 0.5) * 5;
+        var dy = (Math.random() - 0.5) * 5;
+        objectArray.push(new circle(x, y, dx, dy, r));
+    }
+}
+fillExtra();
 //to create an animate loop
 animate = () => {
 
     c.clearRect(0, 0, canvas.width, canvas.height)
-    //c.clearRect(0, 0, canvas.width, canvas.height);
-    //cir.draw();
-    // cir.update();
     for (var i = 0; i < objectArray.length; i++) {
         var DizzyCircle = objectArray[i];
-        DizzyCircle.draw();
         DizzyCircle.update();
 
     }
-    //obejctArray[76].update();
     requestAnimationFrame(animate)
 }
 animate();
